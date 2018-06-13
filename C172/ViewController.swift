275284d52adccb95229dcbc50e88a4c82a727328
Максим Балахонов <gmoaokgsliem@gmail.com>
2018-06-13 16:51:33 +0500

    Initial Commit

---
+++ C172/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  C172
+//
+//  Created by Максим Белугин on 13.06.2018.
+//  Copyright © 2018 Максим Белугин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
